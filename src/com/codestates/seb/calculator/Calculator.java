6d9eda7f2334cc89db938725b584f336350ae5fe
JiHyeong23 <ekdl1120@gmail.com>
2022-11-02 18:39:45 +0900

    calculator

--- src/com/codestates/seb/calculator/Calculator.java
+++ src/com/codestates/seb/calculator/Calculator.java
@@ -3,16 +3,43 @@ import java.util.Scanner;
 
 public class Calculator {
   public static void main(String[] args) {
-    System.out.println("===Java Calculator===");
 
+    double num1;
+    double num2;
+    double result;
+    String operator;
+
+    System.out.println("===Java Calculator===");
     Scanner input = new Scanner(System.in);
-        /*
-            요구 사항에 따라 간단한 계산기를 만들어주세요.
-            1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
-            2. 연산자의 종류는 +, -, *, / 입니다.
-            3. 소수점 연산을 수행할 수 있어야 합니다.
-            4. 연산 결과를 콘솔에 출력합니다.
-        */
-    // TODO...
+
+    System.out.println("숫자를 입력하세요 >");
+    num1 = input.nextDouble();
+    System.out.println("연산자를 입력하세요 >");
+    operator = input.nextLine();
+    operator = input.nextLine();
+    System.out.println("숫자를 입력하세요 >");
+    num2 = input.nextDouble();
+
+    if (operator.equals("+")) {
+      result = num1 + num2;
+      System.out.println("계산 결과 :");
+      System.out.println(result);
+    } else if (operator.equals("-")) {
+      result = num1 - num2;
+      System.out.println("계산 결과 :");
+      System.out.println(result);
+    } else if (operator.equals("*")) {
+      result = num1 * num2;
+      System.out.println("계산 결과 :");
+      System.out.println(result);
+    } else if (operator.equals("/")) {
+      result = num1 / num2;
+      System.out.println("계산 결과 :");
+      System.out.println(result);
+    } else if (operator.equals("%")) {
+      result = num1 % num2;
+      System.out.println("계산 결과 :");
+      System.out.println(result);
+    }
   }
 }
