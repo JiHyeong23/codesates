52243f38a3170109b28e6f4e7ad7f939261b5db6
JiHyeong23 <ekdl1120@gmail.com>
2022-12-15 23:14:59 +0900

    finish

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -1,7 +1,8 @@
 package com.codestates.coffee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -26,5 +27,22 @@ public class CoffeeController {
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
     // 1. 커피 정보 수정을 위한 핸들러 메서드 구현
+    @PutMapping("/{coffee-id}")
+    public ResponseEntity putCoffee(@PathVariable("coffee-id") long coffeeId,
+                                    @RequestParam Map<String, Object> coffee) {
+
+        for(Map.Entry<String, Object> entry : coffee.entrySet()) {
+            coffees.get(coffeeId).put(entry.getKey(), entry.getValue());
+        }
+        return new ResponseEntity(coffees.get(coffeeId), HttpStatus.OK);
+    }
+
     // 2. 커피 정보 삭제를 위한 핸들러 서드 구현
+    @DeleteMapping("/{coffee-id}")
+    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
+
+        coffees.remove(coffeeId);
+
+        return new ResponseEntity(coffees.get(coffeeId), HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -27,6 +27,22 @@ public class MemberController {
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
-    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @PutMapping("/{member-id}")
+    public ResponseEntity putMember (@PathVariable("member-id") long memberId,
+                                     @RequestParam Map<String, Object> member) {
 
+        for(Map.Entry<String, Object> entry : member.entrySet()) {
+            members.get(memberId).put(entry.getKey(), entry.getValue());
+        }
+        return new ResponseEntity(members.get(memberId), HttpStatus.OK);
+    }
+
+    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember (@PathVariable("member-id") long memberId) {
+
+        members.remove(memberId);
+
+        return new ResponseEntity(members.get(memberId), HttpStatus.NO_CONTENT);
+    }
 }
